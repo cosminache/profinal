4c2e2d4fa596b0f495da1a94c2fd691497ce7549
cosminache <csmngheorghi@gmail.com>
2021-08-17 18:45:44 +0300

    deleted some photos

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
     <LeftSection>
       <SectionTitle main center>
         Welcome To <br />
-        My Personal Portofolio
+        My Future Portofolio
       </SectionTitle>
 
       <SectionText>
